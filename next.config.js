2e5949c398f52ef1461c587a42344a6795ef7f81
Mahmood azmii <mahmoodmanuu@gmail.com>
2023-08-21 04:37:59 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+      domains: ["firebasestorage.googleapis.com"],
   },
 };
 
